ec43b895380f21f84debbc6e7a04a06920f8b317
danyirfansyah <danyirfansyah23@gmail.com>
2025-06-13 15:21:40 +0700

    membenarkan vercel dan index

--- api/index.js
+++ api/index.js
@@ -3,15 +3,12 @@ import serverless from 'serverless-http';
 import playerRoutes from '../src/routes/playerRoutes.js';
 
 const app = express();
-
 app.use(express.json());
 
-// Routing utama
 app.use('/api/players', playerRoutes);
 
-// Tes root
 app.get('/', (req, res) => {
-  res.send('Man City API is running ✅');
+  res.send('API is running...');
 });
 
 export const handler = serverless(app);
